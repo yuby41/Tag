43f670523aeb6388c1e7d109815be382ff06e47e
Yualbe Luis <lois21us@gmail.com>
2022-01-18 12:05:50 +0100

    Añadiendo estilos con Tailwind CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,36 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <script src="https://cdn.tailwindcss.com"></script>
         
     </head>
-    <body>
-        <form action="tags" method="POST">
-            @csrf
-            <input type="text" name="name">
-            <input type="submit" name="Agregar">
-        </form>
-
-        <h4>Listado de etiquetas</h4>
-        <table>
-            @forelse ($tags as $tag)
-            <tr>
-                <td>{{ $tag->name }}</td>
-                <td>
-                    <form action="tags/{{ $tag->id }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Eliminar">
-                    </form>
-            </tr>
-                
-            @empty
+    <body class="bg-gray-200 py-10">
+        <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+            <form action="tags" method="POST" class="flex mb-4">
+                @csrf
+                <input type="text" name="name" class="rounded-l bg-gray-200 p-4 w-full outline-none" placeholder="Nueva etiqueta">
+                <input type="submit" name="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none">
+            </form>
+    
+            <h4 class="text-lg text-cente mb-4">Listado de etiquetas</h4>
+            <table>
+                @forelse ($tags as $tag)
                 <tr>
-                    <td>
-                        <p>No hay etiquetas</p>
-                    </td>
+                    <td class="border px-4 py-2">{{ $tag->name }}</td>
+                    <td class="px-4 py-2">
+                        <form action="tags/{{ $tag->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Eliminar" class="px-3 rounded bg-red-500 text-white">
+                        </form>
                 </tr>
-            @endforelse
-        </table>
-
+                    
+                @empty
+                    <tr>
+                        <td>
+                            <p>No hay etiquetas</p>
+                        </td>
+                    </tr>
+                @endforelse
+            </table>
+        </div>
+    
     </body>
 </html>
